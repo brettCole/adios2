8ec296a5bc1fb652d7e1f5148d880322546df9ec
github username <github email address>
2017-08-25 10:58:01 -0400

    created Auth class for JWT gem

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,11 @@
+require 'Auth'
+
 class Api::V1::SessionsController < ApplicationController
 
   def login
     user = User.find_by(email: params[:password])
     if user && user.authenticate(params[:password])
-      render json: { token: Auth.createToke(user)}
+      render json: { token: Auth.create_token({ id: user.id, username: user.username, name: user.name, email: user.email }) }
     else
       render json: { errors: { message: "Unable to find a user with that email or password" } }, status: 500
     end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def register
     user = User.new(user_params)
     if user.save
-      render json: { token: Auth.createToken(user)}
+      render json: { token: Auth.createToken({ id: user.id, username: user.username, name: user.name, email: user.email }) }
     else
       render json: { errors: user.errors.full_messages }, status: 500
     end
@@ -12,7 +12,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :name, :email, :password)
+    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
   end
 
 end
