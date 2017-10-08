8e139af4171066519331e0800bfd2c5d2f479581
github username <github email address>
2017-10-08 16:02:35 -0400

    created redux store

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import loginStatus from './reducers/loginStatus';
+
+const reducers = combineReducers({
+  loginStatus
+});
+
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware)
+);
