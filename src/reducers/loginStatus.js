31176fb5d32d8f603cd30d50373e7a9fdfb60331
github username <github email address>
2017-10-08 16:08:20 -0400

    login action creator

--- src/reducers/loginStatus.js
+++ src/reducers/loginStatus.js
@@ -1,11 +1,24 @@
-export default function loginStatus(state = { isLoggedIn: false, loading: false }, action) {
+export default function loginStatus(state = {
+  id: null,
+  username: null,
+  name: null,
+  email: null,
+  isLoggedIn: false,
+  loading: false}, action) {
   switch(action.type) {
     case 'LOADING':
       return { loading: true }
     case 'RETURN_JWT':
       return { loading: false }
-    case 'LOGGED_IN':
-      return { loading: false, isLoggedIn: true }
+    case 'CURRENT_USER':
+      let user = Object.assign({}, action.payload)
+      return {
+        id: user.id,
+        username: user.username,
+        name: user.name,
+        email: user.email,
+        loading: false,
+        isLoggedIn: true }
     case 'LOGGED_OUT':
       return { loading: false, isLoggedIn: false }
     default:
