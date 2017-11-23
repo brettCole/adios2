7a18dadcbc4698e712eb9f503ca9b975841cd55c
github username <github email address>
2017-11-23 12:44:27 -0500

    add completed item

--- src/reducers/checklist.js
+++ src/reducers/checklist.js
@@ -1,13 +1,29 @@
 export default function createChecklist(state = [], action) {
   switch(action.type) {
     case 'RECEIVE_CHECKLISTS':
-      return Object.assign({}, state, {
-        checklists: action.payload
-      })
-    case 'RETURN_CHECKLISTS':
-      return Object.assign({}, state, {
-        checklists: action.payload
-      })
+      return {
+        checklists: action.payload,
+        completed: action.payload.map((element) => {
+          return element.items.map((each) => {
+            if (each.completed !== true) {
+              return each.completed = false
+            } else { return each.completed = true}
+          })
+        })
+      }
+    case 'CHECKED_OFF_ITEM':
+      // checklists: action.childItem.completed = true
+      // action.childItem.completed = true
+      for (let i = 0; i < action.state.length; i++) {
+        if (action.state[i].id === action.parent.id) {
+          for (let e = 0; e < action.state[i].items.length; e++) {
+            if (action.state[i].items[e].id === action.childItem.id) {
+              action.state[i].items[e] = Object.assign({}, action.state[i].items[e], action.state[i].items[e].completed = true)
+            }
+          }
+        }
+      }
+      return state
     default:
       return state;
   }
