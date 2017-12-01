6a94ac90d6445d0b752f48fc133d64834eca0f61
github username <github email address>
2017-12-01 11:22:09 -0500

    removed show method and and added redirect if no user

--- app/controllers/api/v1/checklists_controller.rb
+++ app/controllers/api/v1/checklists_controller.rb
@@ -2,23 +2,20 @@ class Api::V1::ChecklistsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    user = User.find_by(:id => params[:user_id].to_i)
-    checklist = user.checklists.all
-    render json: checklist, include: [:items], only: [:title, :id], status: 200
+    if current_user
+      user = User.find_by(:id => params[:user_id].to_i)
+      checklist = user.checklists.all
+      render json: checklist, include: [:items], only: [:title, :id], status: 200
+    else
+      redirect "http://localhost:3000/login"
+      render json: { errors: checklist.errors.full_messages }, status: 500
+    end
   end
 
   def create
     checklist = Checklist.new(checklist_params)
     if checklist.valid? && checklist.save
       render json: checklist, only: [:title], status: 200
-    else
-      render json: { errors: checklist.errors.full_messages }, status: 500
-    end
-  end
-
-  def show
-    if checklist = Checklist.find_by(title: params[:title])
-      render json: checklist, only: [:id, :title], status: 200
     else
       render json: { errors: checklist.errors.full_messages }, status: 500
     end
