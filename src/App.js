e11c5aa45d1c80a26eb8502958d58a8591c22c8b
github username <github email address>
2017-12-06 11:28:35 -0500

    removed unnecessary file imports

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import SignupForm from './components/form/SignupForm';
-// import Search from './components/Search'
 import Homepage from './containers/Homepage'
 import LoginForm from './components/form/LoginForm'
 import SearchResultsPage from './containers/SearchResultsPage'
-// import Navigation from './containers/Navigation'
-// import CreateCategory from './components/CreateCategory'
-// import CheckListLink from './components/CheckListLink'
 import ChecklistContainer from './containers/ChecklistContainer'
-// import FlipKey from './components/Flipkey'
-// import Footer from './components/Footer'
-// import ChecklistContainer from './containers/ChecklistContainer'
-
-
-
 
 class App extends Component {
   render() {
